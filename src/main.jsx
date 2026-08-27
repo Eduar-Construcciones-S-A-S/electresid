22c138d8ac7d2a60fbbc0a676d2e88ecea210083
Eduar Construcciones S.A.S <gerenteeduarconstrucciones@gmail.com>
2026-08-26 22:50:19 -0500

    feat: load integrated phase 2 inventory root

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppV2'
+import AppRoot from './AppRoot'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode><AppRoot /></React.StrictMode>
 )
